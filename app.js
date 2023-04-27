095281d4be0333a7876c1ff62737aded3ee14277
HardeepNain <hnspnain@gmail.com>
2023-04-27 11:12:24 +0530

    User joined-leave

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-// We deleted get method kyunki woto bas smjhane ke liye tha ki route kaise work krta h
-// installed socket.io package
+// CODE FLOW jaane ke liye script.js dekho
 
 const express = require("express");
 const app = express();
 
-// socket io ka code : Iss code ko as it is documentation se uthaya h
-// Express initializes app to be a function handler that you can supply to an HTTP server
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io"); // Humne socket.io package se Server(not small s server) naam ki class ko nikal liya
-const io = new Server(server); //  I initialize a new instance of socket.io (jiska naam h io) by passing the server (the HTTP server) object.
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// it makes public folder static : means ki jo bhi files public folder me hogi wo user ko dhikegi inspect krne pe
 app.use(express.static("public"));
 
-/* 
-on == eventListner
-jaise javascript me event attach krne ke liye eventListner likhte the usi prakar se 
-socket.io ke obj pe event lgane ke liye on likha jaata hai
-   
-connection == isko ek type ka event maanlo
-matlab ki jaise hi koi socket aake app se connect hoga to index.html me likhe io() execute hoga
-aur jaha pe bhi on-connection event hoga uska fxn execute hoga
-jo socket is fxn me receive hota h wo wo socket hota hai jo us time aake connect hua hai 
-*/
+let users = []; // user ki list bnali
+
 io.on("connection", function (socket) {
   console.log(socket.id, "Socket connected");
+
+  // socket.on bhi yahi maanlo ek type se event attach krne ke kaam aata hai
+  // on ka matlab hai data consume karna hai
+  // user- connected ek event hai jiske occur hote hi fxn ke andar ka code chlega
+  // jo data receive hoga wo us emit se aaya hoga jispe same event jo yha on pe lga hua h wo lga hua hoga
+  socket.on("user-connected", function (name) {// name emit ne bheja hai
+    users.push({ id: socket.id, name: name });
+    console.log(users);
+
+    // this will emit/send data(name in this case) on all the sockets except the sender
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("disconnect", function () {
+    let disconnectedUser;
+    let filteredUsers = users.filter((userObj) => {
+      if (userObj.id == socket.id) {
+        disconnectedUser = userObj;
+        return false;
+      }
+      return true;
+    });
+    users = filteredUsers;
+    socket.broadcast.emit("user-leave", disconnectedUser.name);
+  });
 });
 
-
-server.listen(4000, function () { // ab app ki jgah server pe listen hoga (even i don't know iska kya matlab h 🤣)
+server.listen(4000, function () {
   console.log("App started at port 4000 !!");
 });
