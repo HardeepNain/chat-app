05d536b979576482144f7237ae04473c1860b33c
HardeepNain <hnspnain@gmail.com>
2023-04-27 11:09:46 +0530

    Understanding Socket.io

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-// Humne yhape express aur nodemon package install kr liye h
+// We deleted get method kyunki woto bas smjhane ke liye tha ki route kaise work krta h
+// installed socket.io package
 
-// express => it is used to create server easily
 const express = require("express");
-
-// server is created : Iss line se server create hoga
 const app = express();
 
-// GET method with route /
-app.get("/" , function(request , response){
-    response.send("<h1>Welcome to My app</h1>");
-})
+// socket io ka code : Iss code ko as it is documentation se uthaya h
+// Express initializes app to be a function handler that you can supply to an HTTP server
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io"); // Humne socket.io package se Server(not small s server) naam ki class ko nikal liya
+const io = new Server(server); //  I initialize a new instance of socket.io (jiska naam h io) by passing the server (the HTTP server) object.
 
-app.listen(4000, function () { // idhar hume btana hota hai ki app kis port pe chalu karni hai
-  console.log("App started at port 4000 !!");
+// it makes public folder static : means ki jo bhi files public folder me hogi wo user ko dhikegi inspect krne pe
+app.use(express.static("public"));
+
+/* 
+on == eventListner
+jaise javascript me event attach krne ke liye eventListner likhte the usi prakar se 
+socket.io ke obj pe event lgane ke liye on likha jaata hai
+   
+connection == isko ek type ka event maanlo
+matlab ki jaise hi koi socket aake app se connect hoga to index.html me likhe io() execute hoga
+aur jaha pe bhi on-connection event hoga uska fxn execute hoga
+jo socket is fxn me receive hota h wo wo socket hota hai jo us time aake connect hua hai 
+*/
+io.on("connection", function (socket) {
+  console.log(socket.id, "Socket connected");
 });
 
-/*
-Humne yhape route de rkha h -> /
-Aur hmara server yaani app listen ho rha h -> localhost: 4000(given port) pe
 
-Iska matlab h ki hum jaise hi apne broweser me localhost:4000 likhenge 
-to humne / route joki by default route hota h uske fxn me response me jo bhi data send kra hoga 
-wo hume browser me as a result show hoga
-
-request : jab bhi hum browser pe hit lgate hai yani localhost:4000 likhe ke to iss application of hit lagti hai 
-aur jo browser se humne hit lgayi hai wo apne saath request naam ka package leke aata hai jisme diff methods hote hai
-*/
-
-/*
-Hum pahle server ko chalu krte h fir request maarte h
-but agr humne server ko chalur krne ke baad code me kuch bhi changes kiye to
-wo changes ko affect me laane ke liye hume pahle server band krna pdega aur fir restart krna pdega
-to taki baar baar changes krne par app ko band krke chalu na krna pde isiliye hum nodemon package use krte h
-jaise hi code me change hoga ye package server ko apne aap restart kr deta h
-
-command to start app normally : node <filename>
-command to start app through nodemon : npm start
-*/
+server.listen(4000, function () { // ab app ki jgah server pe listen hoga (even i don't know iska kya matlab h 🤣)
+  console.log("App started at port 4000 !!");
+});
