0bfc9a67b6559be359935b7032832d4c22d284fa
HardeepNain <hnspnain@gmail.com>
2023-04-27 11:14:42 +0530

    chat-append fxnality

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// CODE FLOW jaane ke liye script.js dekho
-
 const express = require("express");
-const app = express();
 
+const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -10,20 +8,15 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = []; // user ki list bnali
+let users = [];
 
 io.on("connection", function (socket) {
   console.log(socket.id, "Socket connected");
 
-  // socket.on bhi yahi maanlo ek type se event attach krne ke kaam aata hai
-  // on ka matlab hai data consume karna hai
-  // user- connected ek event hai jiske occur hote hi fxn ke andar ka code chlega
-  // jo data receive hoga wo us emit se aaya hoga jispe same event jo yha on pe lga hua h wo lga hua hoga
-  socket.on("user-connected", function (name) {// name emit ne bheja hai
+  socket.on("user-connected", function (name) {
     users.push({ id: socket.id, name: name });
     console.log(users);
 
-    // this will emit/send data(name in this case) on all the sockets except the sender
     socket.broadcast.emit("user-joined", name);
   });
 
@@ -38,6 +31,18 @@ io.on("connection", function (socket) {
     });
     users = filteredUsers;
     socket.broadcast.emit("user-leave", disconnectedUser.name);
+  });
+
+  // app.js ki emitted chat yaha pe receive hogi aur fir ye us user ke name ke saath us chat ko sbko bhej denge
+  socket.on("chat-append", function (chat) {
+    let name;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id == socket.id) { // jis user ki id current jo socket connect hai usse match ho gyi to iska matlab usne hi msg bheja hai to us user ka naam nikal lenge
+        name = users[i].name;
+        break;
+      }
+    }
+    socket.broadcast.emit("append-chat", { name, chat }); //name aur chat dono ko baaki connected sockets ko bhej diya
   });
 });
 
--- public/script.js
+++ public/script.js
@@ -1,8 +1,6 @@
-let name = prompt("Enter Your Name !"); // hum user se aate hi uska naam puch lenge
-
-
+let name = prompt("Enter Your Name !");
 let chatInputBox = document.querySelector(".chat-input");
-let chatWindow = document.querySelector(".chat-window"); 
+let chatWindow = document.querySelector(".chat-window");
 
 chatInputBox.addEventListener("keypress", function (e) {
   if (e.key == "Enter" && chatInputBox.value) {
@@ -11,31 +9,9 @@ chatInputBox.addEventListener("keypress", function (e) {
     chatRight.classList.add("right");
     chatRight.innerHTML = chatInputBox.value;
     chatWindow.append(chatRight);
+
+    socket.emit("chat-append", chatInputBox.value);  // yha se humne chat ko emit kr diya joki app.js me receive hogi
+    
     chatInputBox.value = "";
   }
 });
-
-/*
-CODE FLOW
-
-user aaya h
-
-to jaise hi koi bhi socket connect hoga to io() chlega
-aur socket.js me socket.emit with user-connected event se data send hoga kyunki socket.js file index.html me attached h
-aur app.js me io.on with connection event ka fxn chalega
-user ke liye socket.id aur uske naam ka use krke ek obj bnega aur wo object users array me push hoga
-aur socket.on with user-connected event ke fxn me socket.emit with user-connected event se bheja gya data receive hoga
-fir socket.broadcast.emit with user-joined event ki mdad se user ka name sb connected users ke pass send hoga
-joki socket.js me socket.on with user-joined event ke fxn me receive hoga
-jaha pe us name ka use krke ek div create hoga aur wo div chats wale div pe append hoga
-
-Use jaa rha h
-
-jaise hi socket disconnect hoga to app.js me socket.on with disconnect event me likha fxn execute hoga
-aur users array pe filter lgega jisme ek ek krke user ke obj pass honge
-aur jis user ki id socket ki id se match ho jaayegi matlab wo user chhod ke gya hai
-us user ko users array me se nikal denge
-aur socket.broadcast.emit with user-leave event us user ka name send kr dega baki connected sockets ko
-joki socket.js me socket.on with user-leave event ke fxn me receive hoga
-jaha pe us name ka use krke ek div create hoga aur wo div chats wale div pe append hoga
-*/
